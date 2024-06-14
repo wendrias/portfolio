56b8d00a63508652e6bdd5b274cebc2e9147a4fb
wendrias <39842984+wendrias@users.noreply.github.com>
2024-06-13 20:09:16 -0400

    react-router + responsive design additions

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,39 +1,3 @@
-// import React, { useState } from "react";
-
-// interface NavProps {
-//   logoTheme: string;
-//   navTheme: string;
-// }
-
-// function Navbar({ logoTheme, navTheme }: NavProps) {
-//   return (
-//     <nav className={"nav"}>
-//       <a className="nav-heading" href="/">
-//         <img src={"./img/logo-" + logoTheme + ".png"} />
-//       </a>
-
-//       <ul className={"nav-areas nav-" + navTheme}>
-//         <li>
-//           <a className="nav-link" href="/portfolio/">
-//             01. Home
-//           </a>
-//         </li>
-//         <li>
-//           <a className="nav-link" href="/portfolio/projects">
-//             02. Projects
-//           </a>
-//         </li>
-//         <li>
-//           <a className="nav-link" href="/portfolio/about">
-//             03. About
-//           </a>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// }
-// export default Navbar;
-
 import React from "react";
 import { Link } from "react-router-dom";
 
